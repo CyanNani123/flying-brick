8ae9f844060da115293e3efaaf090b018367f506
CyanNani123 <f.st@live.de>
2019-05-05 22:59:21 +0200

    screen dimensions are now changeable via constructur so screen class can be reused easily

--- screen.py
+++ screen.py
@@ -2,10 +2,10 @@ from flyingbrick import *
 import pygame
     
 class Screen():
-    def __init__(self):
+    def __init__(self,width=400,height=600):
         '''initialize pygame screen'''
         pygame.init
-        self.screen = pygame.display.set_mode((400,600))
+        self.screen = pygame.display.set_mode((width,height))
         self.screen.fill(pygame.Color(255,255,255))
         pygame.display.init()
         pygame.display.update()
